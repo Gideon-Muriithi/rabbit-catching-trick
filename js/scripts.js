ad08a3da7af84c0b50eccff5d88a2874c7918fc5
Gideon-Muriithi <gideongakenge@gmail.com>
2019-07-17 13:00:06 +0300

    Add changes

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+$(".clickable").click(function(){
+  $("#text-hidden1").show();
+  });
+$(".clickable1").click(function(){
+    $("#text-hidden2").show();
+    });
+$(".clickable2").click(function(){
+    $("#text-hidden3").show();
+  });
+});
+//$(".clickable").click(function(){
+//  $(".clickable").show();
+//});
+//});
